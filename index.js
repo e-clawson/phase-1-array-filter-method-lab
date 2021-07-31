8430a17f1e832ecf0e37c808e840a8d267257c36
Elizabeth Clawson <emclawson1@gmail.com>
2021-07-31 17:16:32 -0500

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const newDrivers = drivers.filter(function(person){return person.toLowerCase() === name.toLowerCase()});
+    return newDrivers;
+}
+function fuzzyMatch(drivers, name) { {
+        return drivers.filter(
+          (possibleMatch) =>
+            possibleMatch.toLowerCase().indexOf(name.toLowerCase()) === 0
+        );
+      }
+}
+
+function matchName(drivers, name){ 
+    return drivers.filter(function(driver){
+        return driver.name === name
+        });
+}
